e916242fe59f33cb23f9e85059df00e298862f19
Cruise Brantley <cruise.brantley@gmail.com>
2021-04-12 01:54:16 -0500

    Updated to work with single server path

--- src/Container.js
+++ src/Container.js
@@ -1,10 +1,12 @@
 import React, { useState, useEffect } from "react";
 import Server from "./ServerList";
 import Results from "./Results";
+import { useLocation } from "react-router";
 
 export default function Container({ data }) {
   const [servers, setServers] = useState([]);
   const [currentServer, setCurrentServer] = useState();
+  const [location] = useState(useLocation().pathname.substring(1));
 
   useEffect(() => mapServers(data), [data]);
 
@@ -16,21 +18,39 @@ export default function Container({ data }) {
     setServers(tempServers);
   };
 
+  const renderOneServer = () => {
+    if (currentServer !== location) setCurrentServer(location)
+    return <Server
+      key={location}
+      server={location}
+      onClick={() => { }}
+      isHighlighted={true}
+    />
+  }
+
+  const renderServers = () => (
+    servers.map(server => (
+      <Server
+        key={server}
+        server={server}
+        onClick={() =>
+          currentServer === server
+            ? setCurrentServer(undefined)
+            : setCurrentServer(server)
+        }
+        isHighlighted={server === currentServer}
+      />
+    ))
+  )
+
   return (
     <div className="container">
       <div className="left-pane">
-        {servers.map(server => (
-          <Server
-            key={server}
-            server={server}
-            onClick={() =>
-              currentServer === server
-                ? setCurrentServer(undefined)
-                : setCurrentServer(server)
-            }
-            isHighlighted={server === currentServer}
-          />
-        ))}
+        {
+          servers.find(server => server === location) ?
+            renderOneServer() :
+            renderServers()
+        }
       </div>
       <Results
         data={data && data[currentServer]}
