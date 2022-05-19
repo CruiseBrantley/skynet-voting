6798dbe4837a53571769906abe911f6600dfc05c
Cruise Brantley <cruise.brantley@gmail.com>
2022-05-19 02:22:30 -0500

    removed excess line

--- src/Results.js
+++ src/Results.js
@@ -20,7 +20,6 @@ export default function Results ({ data, currentServer }) {
 
   return (
     <div className='right-pane'>
-      {console.log(highest)}
       {data ? (
         <>
           <VictoryBar
