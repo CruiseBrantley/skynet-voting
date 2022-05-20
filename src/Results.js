b57b87d3135311e8be2d629b09ad16d8eb26cbe1
Cruise Brantley <cruise.brantley@gmail.com>
2022-05-20 12:28:50 -0500

    Updates

--- src/Results.js
+++ src/Results.js
@@ -2,7 +2,15 @@ import React, { useState, useEffect } from 'react'
 import { VictoryBar } from 'victory'
 import CustomLabel from './CustomLabel'
 
-export default function Results ({ data, currentServer }) {
+const replaceAll = originalStr => {
+  const replaceStrArr = ['(NES)', '(SNES)', '(N64)']
+  for (let str of replaceStrArr) {
+    originalStr = originalStr.replace(str, '')
+  }
+  return originalStr.trim()
+}
+
+export default function Results ({ data }) {
   const [highest, setHighest] = useState(0)
 
   useEffect(() => {
@@ -13,7 +21,7 @@ export default function Results ({ data, currentServer }) {
     return data
       .map(game => {
         if (game.hasVoted?.length > highest) setHighest(game.hasVoted.length)
-        return { x: game.title, y: game.hasVoted ? game.hasVoted.length : 0 }
+        return { x: replaceAll(game.title), y: game.hasVoted ? game.hasVoted.length : 0 }
       })
       .reverse()
   }
@@ -28,14 +36,14 @@ export default function Results ({ data, currentServer }) {
             labels={({ datum }) => datum.x}
             horizontal
             animate={{
-              duration: 500,
-              onLoad: { duration: 500 },
-              onchange: { duration: 500 }
+              duration: 250,
+              onLoad: { duration: 250 },
+              onchange: { duration: 250 }
             }}
             style={{
               data: {
                 fill: ({ datum }) => (datum.y >= highest ? 'gold' : 'teal'),
-                width: 10
+                width: 12
               },
               labels: {
                 fill: ({ datum }) => (datum.y >= highest ? 'crimson' : 'white'),
