2aaf5e43978eb54dc88dcca64a499401ede13934
Cruise Brantley <cruise.brantley@gmail.com>
2022-05-20 23:52:00 -0500

    Better reference

--- src/CustomLabel.js
+++ src/CustomLabel.js
@@ -3,7 +3,7 @@ import { VictoryLabel } from 'victory'
 const CustomLabel = props => {
   return (
     <>
-      <VictoryLabel {...props} x={385} textAnchor='end' text={(data) => data.datum.y} />
+      <VictoryLabel {...props} x={385} textAnchor='end' text={({datum}) => datum.y} />
       <VictoryLabel {...props} x={200} textAnchor='middle' />
     </>
   )
